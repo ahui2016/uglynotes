9641bd4bf2a94cde258aad372f98b53b5014d66b
ahui <ahui.2015@ilcoud.com>
2020-12-31 13:04:00 +0800

    add history.html

---
+++ static/history.js
@@ -0,0 +1,45 @@
+const id = getUrlParam('id');
+const form = new FormData();
+form.append('id', id);
+
+ajaxPost(form, '/api/history', null, that => {
+  const history = that.response;
+  const createdAt = dayjs(history.CreatedAt);
+  $('#datetime').text(createdAt.format('YYYY-MM-DD HH:mm:ss'));
+  $('#note-id')
+    .text('id:'+history.NoteID)
+    .attr('href', '/html/note?id='+history.NoteID);
+  $('#history-id').text(history.ID);
+  $('#size').text(fileSizeToString(history.Size));
+
+  const protected = $('#protected');
+  const protectBtn = $('#protect');
+  const unprotectBtn = $('#unprotect');
+  protectBtn.click(protectToggle);
+  unprotectBtn.click(protectToggle);
+  if (history.Protected) protectToggle();
+
+  function protectToggle() {
+    protected.toggle();
+    protectBtn.toggle();
+    unprotectBtn.toggle();
+  }
+
+  const plaintext = $('.plaintext.contents');
+  plaintext.text(history.Contents);
+
+  const markdown = $('.markdown.contents');
+  const dirty = marked(history.Contents);
+  const clean = DOMPurify.sanitize(dirty);
+  markdown.html(clean);
+
+  $('input[name="note-type"]').change(() => {
+    plaintext.toggle();
+    markdown.toggle();
+  });
+  
+}, function() {
+  //onloadend
+  $('#loading').hide();
+});
+  
